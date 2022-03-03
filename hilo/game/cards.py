01081505d81b4e1786fafd26ef773f2494aab454
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-03 09:11:10 -0700

    fixed cards class

--- hilo/game/cards.py
+++ hilo/game/cards.py
@@ -3,13 +3,11 @@ import random
 class Cards():
     def __init__(self):
         self.deck = self.new_deck()
-        print(self.deck)
 
     def draw(self):
         if len(self.deck) == 0:
             self.deck = self.new_deck()
         index = random.randint(1, len(self.deck)) - 1
-        print(self.deck[index])
         
         return self.deck.pop(index)
 
