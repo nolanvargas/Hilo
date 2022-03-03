adf4cd5178e292d900d55974f1087e14e6c45105
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-03 09:01:58 -0700

    fixed draw() bug

--- hilo/game/cards.py
+++ hilo/game/cards.py
@@ -6,11 +6,12 @@ class Cards():
         print(self.deck)
 
     def draw(self):
-        index = random.randint(1, len(self.deck)) - 1
-        print(self.deck[index])
-        self.deck.pop(index)
         if len(self.deck) == 0:
             self.deck = self.new_deck()
+        index = random.randint(1, len(self.deck)) - 1
+        print(self.deck[index])
+        
+        return self.deck.pop(index)
 
     def new_deck(self):
         cards = []
